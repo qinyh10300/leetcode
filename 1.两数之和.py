b4cc527fdf38e6fddfbbcf49b815dbeb85003cd2
Qin Yihua <qinyh10300@gmail.com>
2025-06-10 23:39:22 +0800

    first commit

---
+++ 1.两数之和.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode.cn id=1 lang=python3
+#
+# [1] 两数之和
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            # print(target - nums[i], dict, (target - nums[i]) in dict)
+            if (target - nums[i]) in dict:
+                return dict[target - nums[i]], i
+            else:
+                dict[nums[i]] = i
+# @lc code=end
+
